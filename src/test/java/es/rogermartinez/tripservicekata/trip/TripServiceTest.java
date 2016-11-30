dc17df92d3886a879d35b0bccb8de5c45e64c3eb
Roger Martínez <roger.runch@gmail.com>
2016-11-30 10:28:09 +0100

    Fix test: should_throw_an_exception_when_user_is_not_logged_in, extract static reference

--- src/test/java/es/rogermartinez/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/es/rogermartinez/tripservicekata/trip/TripServiceTest.java
@@ -1,6 +1,7 @@
 package es.rogermartinez.tripservicekata.trip;
 
 import es.rogermartinez.tripservicekata.exception.UserNotLoggedInException;
+import es.rogermartinez.tripservicekata.user.User;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,8 +10,15 @@ public class TripServiceTest {
 
     @Test(expected = UserNotLoggedInException.class)
     public void should_throw_an_exception_when_user_is_not_logged_in() throws Exception {
-        TripService tripService = new TripService();
+        TripService tripService = new TestableTripService();
 
         tripService.getTripsByUser(null);
+    }
+
+    private class TestableTripService extends TripService {
+        @Override
+        protected User getLoggedUser() {
+            return null;
+        }
     }
 }
