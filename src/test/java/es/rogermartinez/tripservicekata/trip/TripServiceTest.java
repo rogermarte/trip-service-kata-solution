311182c6f664f1f244286a3b2ae2ed31c082164f
Roger Martínez <roger.runch@gmail.com>
2016-11-30 14:37:47 +0100

    Create the @Before method

--- src/test/java/es/rogermartinez/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/es/rogermartinez/tripservicekata/trip/TripServiceTest.java
@@ -2,23 +2,46 @@ package es.rogermartinez.tripservicekata.trip;
 
 import es.rogermartinez.tripservicekata.exception.UserNotLoggedInException;
 import es.rogermartinez.tripservicekata.user.User;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class TripServiceTest {
 
     private static final User GUEST = null;
     private static final User UNUSED_USER = null;
+    private static final User REGISTERED_USER = new User();
+    private static final User ANOTHER_USER = new User();
+    private static final Trip TO_BRAZIL = new Trip();
     private User loggedInUser;
+    private TripService tripService;
+    
+    @Before
+    public void setUp() throws Exception {
+        tripService = new TestableTripService();
+    }
 
     @Test(expected = UserNotLoggedInException.class)
     public void should_throw_an_exception_when_user_is_not_logged_in() throws Exception {
-        TripService tripService = new TestableTripService();
-
         loggedInUser = GUEST;
 
         tripService.getTripsByUser(UNUSED_USER);
+    }
+
+    @Test
+    public void should_not_return_any_trips_when_users_are_not_friends() throws Exception {
+        loggedInUser = REGISTERED_USER;
+        User friend = new User();
+        friend.addFriend(ANOTHER_USER);
+        friend.addTrip(TO_BRAZIL);
+
+        List<Trip> trips = tripService.getTripsByUser(friend);
+
+        assertThat(trips.size(), is(0));
     }
 
     private class TestableTripService extends TripService {
