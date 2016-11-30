d0e6393615928dc3f310c1065d87b5359d74b445
Roger Martínez <roger.runch@gmail.com>
2016-11-30 16:27:34 +0100

    Refactor to a Guard Clause

--- src/main/java/es/rogermartinez/tripservicekata/trip/TripService.java
+++ src/main/java/es/rogermartinez/tripservicekata/trip/TripService.java
@@ -1,25 +1,26 @@
 package es.rogermartinez.tripservicekata.trip;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import es.rogermartinez.tripservicekata.exception.UserNotLoggedInException;
 import es.rogermartinez.tripservicekata.user.User;
 import es.rogermartinez.tripservicekata.user.UserSession;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TripService {
 
     public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-        List<Trip> tripList = new ArrayList<Trip>();
         User loggedInUser = getLoggedUser();
-        if (loggedInUser != null) {
-            if (user.isFriendWith(loggedInUser)) {
-                tripList = tripsBy(user);
-            }
-            return tripList;
-        } else {
+        if (loggedInUser == null) {
             throw new UserNotLoggedInException();
         }
+
+        List<Trip> tripList = new ArrayList<Trip>();
+        if (user.isFriendWith(loggedInUser)) {
+            tripList = tripsBy(user);
+        }
+        return tripList;
+
     }
 
     protected List<Trip> tripsBy(User user) {
