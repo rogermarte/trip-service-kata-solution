bfe41493ed19a9d3a7969a9b3bf6d825755786fb
Roger Martínez <roger.runch@gmail.com>
2016-11-30 15:56:00 +0100

    Fix code to pass test should inform when users are friends

--- src/test/java/es/rogermartinez/tripservicekata/user/UserTest.java
+++ src/test/java/es/rogermartinez/tripservicekata/user/UserTest.java
@@ -1,15 +1,11 @@
 package es.rogermartinez.tripservicekata.user;
 
-import es.rogermartinez.tripservicekata.trip.UserBuilder;
-import javafx.beans.binding.BooleanBinding;
 import org.junit.Test;
 
+import static es.rogermartinez.tripservicekata.trip.UserBuilder.aUser;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
-/**
- * Created by rmartinez on 30/11/2016.
- */
 public class UserTest {
 
     private static final User BOB = new User();
@@ -17,16 +13,16 @@ public class UserTest {
 
     @Test
     public void should_inform_when_users_are_not_friends() throws Exception {
-        User user = UserBuilder.aUser()
-                                    .friendWith(BOB)
-                                    .build();
+        User user = aUser()
+                .friendWith(BOB)
+                .build();
 
         assertThat(user.isFriendWith(PAUL), is(false));
     }
 
     @Test
     public void should_inform_when_users_are_friends() throws Exception {
-        User user = UserBuilder.aUser()
+        User user = aUser()
                 .friendWith(BOB, PAUL)
                 .build();
 
